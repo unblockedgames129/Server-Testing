6aff50e8ab79063fa35e97dd535bb9e4f8efeb5f
unblockedgames129 <110648595+unblockedgames129@users.noreply.github.com>
2023-10-04 08:35:50 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
-bash ngrok config add-authtoken 2WIZtFYoAjbZouD1nOmzuAmNHOW_2jXJHiXsQPsUWHhrkX3YB
+chmod +x ngrok
+./ngrok config add-authtoken 2WIZtFYoAjbZouD1nOmzuAmNHOW_2jXJHiXsQPsUWHhrkX3YB
 npx gritty --port 8000 --command "bash boot.sh" --auto-restart
-bash ngrok http 8000
+./ngrok http 8000
